db84b4d4aee0d1ea9ad2317998a1a04c5f8adc54
Bagus_24 <Bagus_24@Bagus24>
2020-10-15 19:57:27 +0700

    new commit

---
+++ src/pboif2/pkg10119064/latihan15/logika/PBOIF210119064Latihan15Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini berisi program untuk melakukan operasi Logika
+ */
+public class PBOIF210119064Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "  + (a && b));
+        System.out.println("a || b = "  + (a || b));
+        System.out.println("!a = "  + !a);
+        System.out.println("!(a && b) = "  + !(a && b));
+    }
+    
+}
